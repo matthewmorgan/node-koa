b912e4c49941114b4fa94ef5e52b48fc83d7f846
Matthew Morgan <matt@dulcetsoftware.com>
2017-12-28 10:35:44 -0500

    Clean up code

--- routes/koala.js
+++ routes/koala.js
@@ -4,8 +4,11 @@ const store = require('../db/object-store')
 
 router.get(
     '/koala',
-    async (ctx, next) => await ctx.render ('hello', {items: (await store.listIds({Collection: 'YPT_Database', pageLimit: 1})).ids})
+    async (ctx, next) => await ctx.render ('hello', {items: await fetchIds()})
 )
 
+async function fetchIds(){
+  return (await store.listIds({Collection: 'YPT_Database', pageLimit: 1})).ids
+}
 
 module.exports = router
